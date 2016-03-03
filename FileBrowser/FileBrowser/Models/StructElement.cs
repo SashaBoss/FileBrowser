f69cd8f7bcf0eedec01919522dd846bf1f241231
SashaBoss <hiphopkrv@gmail.com>
2016-03-03 16:05:41 +0200

    initial

---
+++ FileBrowser/FileBrowser/Models/StructElement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace FileBrowser.Models
+{
+    public class StructElement
+    {
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+        
+        [JsonProperty(PropertyName = "type")]
+        public string Type { get; set; }
+
+        [JsonProperty(PropertyName = "path")]
+        public string Path { get; set; }
+
+        [JsonProperty(PropertyName = "items")]
+        public List<StructElement> Items { get; set; }
+    }
+
+    public enum FileType
+    {
+        File,
+        Folder
+    }
+}
